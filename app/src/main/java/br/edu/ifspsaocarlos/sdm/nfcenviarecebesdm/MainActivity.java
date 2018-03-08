52220a10886ccaf7fb316862380c4ad0e1e4b5a1
northonh <nobile.pedro@gmail.com>
2018-03-08 16:29:27 -0300

    Implementado o envio de uma mensagem.

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/nfcenviarecebesdm/MainActivity.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/nfcenviarecebesdm/MainActivity.java
@@ -1,8 +1,12 @@
 package br.edu.ifspsaocarlos.sdm.nfcenviarecebesdm;
 
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.support.v7.app.AppCompatActivity;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -35,8 +39,51 @@ public class MainActivity extends AppCompatActivity {
         // Checando se o Adaptador NFC existe
         adaptadorNFC = NfcAdapter.getDefaultAdapter(this);
         if (adaptadorNFC == null) {
-            Toast.makeText(this, "Adaptador NFC necessário", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, R.string.adaptador_nfc_necessario, Toast.LENGTH_SHORT).show();
             finish();
         }
     }
+
+    /* Trata o clique no botão, travando os elementos de UI até que a mensagem seja enviada */
+    public void preparaEnvio(View view) {
+        if (view.getId() == R.id.bt_prepara_envio) {
+            // Prepara o envio da mensagem
+            criaMensagemNdef();
+
+            // Trava os elementos de UI
+            ativaUI(false);
+            Toast.makeText(this, R.string.mensagem_preparada, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void ativaUI(boolean estado) {
+        mensagemEnviarET.setEnabled(estado);
+        preparaEnvioButton.setEnabled(estado);
+    }
+
+    private void criaMensagemNdef() {
+        /* 2 porque o primeiro vai ser o registro do aplicativo e o segundo a mensagem de fato */
+        NdefRecord[] registrosNdef = new NdefRecord[2];
+
+        /* O registro do aplicativo pode estar em qualquer posição do vetor e indica ao receptor
+        *  qual aplicativo queremos que receba o  ACTION_NDEF_DISCOVERED. Podem existir vários
+        *  registros de aplicativo. Se nenhum for encontrado, o Google Play é aberto para a
+        *  instalação*/
+        registrosNdef[0] = NdefRecord.createApplicationRecord(getPackageName());
+
+        String mensagem = mensagemEnviarET.getText().toString();
+        NdefRecord registroNdefMensagem = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], mensagem.getBytes());
+        registrosNdef[1] = registroNdefMensagem;
+
+        /* Criando a mensagem NDEF com os registros acima */
+        NdefMessage mensagemNdef = new NdefMessage(registroNdefMensagem);
+
+        /* Passando a mensagem para o Adaptador para que seja enviada quando um dispositivo NFC
+        * for encontrado. Também é possível criar a mensagem somente quando um
+        * ACTION_NDEF_DISCOVERED for lançado. Para isso é necessário implementar as Interfaces
+        * NfcAdapter.CreateNdefMessageCallback (método createNdefMessage) e
+        * NfcAdapter.OnNdefPushCompleteCallback (método onNdefPushComplete) */
+        adaptadorNFC.setNdefPushMessage(mensagemNdef, this);
+    }
+
 }
